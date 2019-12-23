df8965de0b8402a56135e7f3766b7154fa1c6364
yuezijian <luna.yzj@icloud.com>
2019-12-23 15:18:49 +0800

    Edit CMakeLists.txt.

--- client/main.cpp
+++ client/main.cpp
@@ -1,8 +1,10 @@
 #include <QtWidgets>
 
-#include <ActiveQt/QAxWidget>
-
 #include "MainWindow.hpp"
+
+#ifdef _WIN32
+    #include <ActiveQt/QAxWidget>
+#endif
 
 
 int main( int argc, char* argv[] )
@@ -19,20 +21,26 @@ int main( int argc, char* argv[] )
 
     QApplication application( argc, argv );
 
-    QAxWidget widget;
-
-    widget.setControl( "{6F54E999-11EF-45DC-9E58-2858314C7016}" );
-    widget.show();
-
-    //MainWindow window;
-
-    //window.ToURL( "http://10.68.4.11/webcomm" );
-    //window.showMaximized();
+    #ifdef _WIN32
 
     //if ( QLibrary::isLibrary( "editor.dll" ) )
     //{
     //    int i = 0;
     //}
+
+    QAxWidget widget;
+
+    widget.setControl( "{6F54E999-11EF-45DC-9E58-2858314C7016}" );
+    widget.show();
+
+    #else
+
+    MainWindow window;
+
+    window.ToURL( "http://10.68.4.11/webcomm" );
+    window.showMaximized();
+
+    #endif
 
     return QApplication::exec();
 }
