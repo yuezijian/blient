f2bbf79ecf8b1ccbd000057d603b6d47dc72f7cb
yuezijian <luna.yzj@icloud.com>
2020-01-16 08:43:45 +0800

    Add some missing files.

---
+++ client/_client.hpp
@@ -0,0 +1,20 @@
+//
+// Created by 岳子剑 on 2020/01/09.
+//
+
+
+#ifndef BLIENT__CLIENT__HPP
+#define BLIENT__CLIENT__HPP
+
+
+#ifndef BLIENT_CLIENT__EXPORT
+
+    #define BLIENT_CLIENT__EXPORT
+
+    //#define BLIENT_CLIENT__EXPORT __attribute__((visibility( "default")))
+    //#define BLIENT_CLIENT__IMPORT __attribute__((visibility( "default")))
+
+#endif
+
+
+#endif
